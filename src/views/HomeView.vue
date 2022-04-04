f179801f4208d3bf70a4d950da693aacae01e54f
Zarantonello <andrea.zarantonello@accenture.com>
2022-04-04 16:34:33 +0200

    add tablet laptop and desktop template page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,22 @@
 <template>
-  <Navbar />
+  <PrivatePageTemplate>
+    <p>Hello</p>
+  </PrivatePageTemplate>
+
+  <!-- <Navbar /> -->
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
   // COMPONENTS
-  import Navbar from '@/components/organisms/Navbar.vue';
+  // import Navbar from '@/components/organisms/Navbar.vue';
+  import PrivatePageTemplate from '@/components/organisms/PrivatePageTemplate.vue';
 
   export default Vue.extend({
     name: 'HomeViewasdasd',
     components: {
-      Navbar
+      // Navbar,
+      PrivatePageTemplate
     }
   })
 </script>
