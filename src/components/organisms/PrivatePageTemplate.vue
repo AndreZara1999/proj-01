9eb573e804de331dbf56c6389d5a696d0f90dc13
Andrea Zarantonello <andrea.zarantonello99@gmail.com>
2022-04-04 22:14:48 +0100

    fix the width of content page section

--- src/components/organisms/PrivatePageTemplate.vue
+++ src/components/organisms/PrivatePageTemplate.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="hidden md:flex w-full h-screen p-3 flex-row flex-nowrap bg-slate-800">
+    <div class="hidden md:flex w-full h-screen p-3 flex-row flex-nowrap bg-slate-800 page-wrapper">
         <section class="md:w-1/6 lg:w-1/4 xl:w-1/5 2xl:w-1/6 2xl:max-w-xs p-5 md:mr-1 lg:mr-5 xl:mr-10 shadow-lg bg-indigo-600 rounded-md">
             <NavigationHead />
         </section>
-        <section class="md:w-5/6 lg:w-3/4 xl:w-4/5 2xl:w-5/6 shadow-lg bg-slate-700 rounded-md">
+        <section class="w-full shadow-lg bg-slate-700 rounded-md">
             <slot></slot>
         </section>
     </div>
@@ -21,3 +21,10 @@
         }
     });
 </script>
+
+<style scoped>
+    .page-wrapper {
+        max-width: 95%;
+        margin: 0 auto;
+    }
+</style>
