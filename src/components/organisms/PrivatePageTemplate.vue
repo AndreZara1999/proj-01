cf7c536c2a04fca340999fd3442a4b9bad6f026b
AndreZara1999 <56677021+AndreZara1999@users.noreply.github.com>
2022-04-05 22:30:41 +0200

    update the width

--- src/components/organisms/PrivatePageTemplate.vue
+++ src/components/organisms/PrivatePageTemplate.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="hidden md:flex w-full h-screen p-3 flex-row flex-nowrap bg-slate-800">
-        <section class="md:w-1/6 lg:w-1/4 xl:w-1/5 2xl:w-1/6 2xl:max-w-xs p-5 md:mr-1 lg:mr-5 xl:mr-10 relative text-center shadow-lg bg-indigo-600 rounded-md">
+        <section class="w-[20%] p-5 mr-3 relative text-center shadow-lg bg-indigo-600 rounded-md">
             <NavigationHead />
             <NavigationBody />
             <div class="absolute bottom-[25px] right-[25px]">
